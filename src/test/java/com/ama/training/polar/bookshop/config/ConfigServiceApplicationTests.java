2a860434b6476875c7fc0d81b2fe6638c408c739
Adrian Marc <adrian.marc95@gmail.com>
2023-03-18 16:57:02 +0200

    Initial Configuration Service

---
+++ src/test/java/com/ama/training/polar/bookshop/config/ConfigServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ama.training.polar.bookshop.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
